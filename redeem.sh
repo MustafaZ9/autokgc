55934a6e5de823329270eec6dceca39e69aa397f
MustafaZ9 <247871525+MustafaZ9@users.noreply.github.com>
2026-08-08 15:34:11 +0300

    Modify redeem script to include kingdom argument
    Added '--kingdom 1266' argument to python script call.

--- redeem.sh
+++ redeem.sh
@@ -14,7 +14,7 @@ source venv/bin/activate
 # 3. Run the python script
 # Using "$1" passes the first word you type after the script name as the code
 echo "🚀 Redeeming code: $1..."
-python redeem_codes.py --csv lod.csv --code "$1"
+python redeem_codes.py --csv lod.csv --code "$1" --kingdom 1266
 
 # 4. Deactivate the venv when done (optional, keeps your terminal clean)
 deactivate
